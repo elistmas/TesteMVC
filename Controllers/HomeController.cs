9ba7fac94f3ca4430ec965bc85db4c7eb35aaf19
elistmas <eli_stmas@hotmail.com>
2022-04-28 16:36:43 -0300

    Segundo commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace FirstMVC.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
